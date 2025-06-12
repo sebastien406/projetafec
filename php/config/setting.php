767323c1db979bed9981e20af6e3686332dbdeb0
seb <niveletsebastien84@gmail.com>
2025-06-12 12:22:14 +0200

    error

--- php/config/setting.php
+++ php/config/setting.php
@@ -1,5 +1,11 @@
 
 <?php
+
+ini_set('display_errors', '1');
+ini_set('error_reporting', E_ALL);
+ini_set('display_startup_errors', '1');
+ini_set('log_errors', '1');
+
 // Lire le fichier .env
 function loadEnv($path)
 {
