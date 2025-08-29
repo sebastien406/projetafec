c059fd60f5f2a54025a6030e0e2c4642d049a8e0
seb <niveletsebastien84@gmail.com>
2025-08-29 18:00:51 +0200

    oups

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -25,21 +25,20 @@
 //         });
 //     }
 // });
-// Fonction pour ouvrir la modale
+// ---------------------------------------------------------------------------------------------------
 export function openModal(reviewModal) {
   if (reviewModal) {
     reviewModal.classList.add('active');
   }
 }
 
-// Fonction pour fermer la modale
+
 export function closeModal(reviewModal) {
   if (reviewModal) {
     reviewModal.classList.remove('active');
   }
 }
 
-// Fonction pour configurer les écouteurs d'événements
 export function setupModal() {
   const openModalBtn = document.getElementById('openModalReviewBtn');
   const reviewModal = document.getElementById('reviewModal');
@@ -62,5 +61,4 @@ export function setupModal() {
   }
 }
 
-// Appeler setupModal quand le DOM est chargé
 document.addEventListener('DOMContentLoaded', setupModal);
